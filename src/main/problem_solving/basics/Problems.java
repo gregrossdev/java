d8e2e56dab838e97b5d740ae88621f73250d89a5
Greg Ross <gregross.dev@gmail.com>
2024-03-20 09:29:30 -0400

    loops used to access items of an array (collection)

--- src/main/problem_solving/basics/Problems.java
+++ src/main/problem_solving/basics/Problems.java
@@ -75,6 +75,23 @@ public class Problems {
         }
     }
 
+    static class Loops {
+        /*
+         Given an array of ints length 3, return the sum of all the elements.
 
+         TDD:
+         sum3([1, 2, 3]) → 6
+         sum3([5, 11, 2]) → 18
+         sum3([7, 0, 0]) → 7
+         */
+        public int sum3(int[] nums) {
+            int sum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
+
+            return sum;
+        }
+    }
 
 }
